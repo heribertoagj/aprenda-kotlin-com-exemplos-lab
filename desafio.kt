ea8873a9d9a7ac032b9463edd7c73264e9c5d5b6
heribertoagj <60655632+heribertoagj@users.noreply.github.com>
2023-04-21 23:27:36 -0300

    abstraindo problema proposto no desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+data class Usuario(var nome: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun getUsuariosInscritos():List<Usuario>{
+        return inscritos
+    }
+
+    fun duracaoTotal(): Int {
+        var horas = 0
+        for (conteudo in conteudos) horas += conteudo.duracao
+        return horas
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usr1 = Usuario("Jonh")
+    val usr2 = Usuario("mary")
+
+    val conteudo1 = ConteudoEducacional("Aprendendo Kotlin",1, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Desafio Kotlin",2, Nivel.BASICO)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2))
+    formacaoKotlin.matricular(usr1)
+    formacaoKotlin.matricular(usr2)
+
+    println("Formação kotlin")
+    for (conteudo in formacaoKotlin.conteudos){
+        println("${conteudo.nome}  |  ${conteudo.duracao} hrs  | ${conteudo.nivel}")
+    }
+
+    println( "Inscritos")
+    for (usuario in formacaoKotlin.getUsuariosInscritos()){
+        println("Nome: ${usuario.nome}")
+    }
 }
